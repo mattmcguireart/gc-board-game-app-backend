fba25acfa63a1163566633a2d5dbf64eeb27a76f
Matt <mattmcguireart@gmail.com>
2021-08-03 11:58:54 -0400

    game list back

--- functions/src/routes/appRouter.ts
+++ functions/src/routes/appRouter.ts
@@ -1,8 +1,14 @@
 import express from "express";
 import { getClient } from "../db";
+import Game from "../models/Game";
 import appRouter from "../models/Game";
 
 const appRouter = express.Router();
+
+const catchError = (err: any, res: any) => {
+  console.error("FAIL", err);
+  res.status(500).json({ message: "internal server error" });
+};
 
 appRouter.get("/", async (req, res) => {
   const client = await getClient();
@@ -14,4 +20,14 @@ appRouter.get("/", async (req, res) => {
   res.json(results);
 });
 
+appRouter.post("/", async (req, res) => {
+  const newSavedGame: Game = req.body;
+  try {
+    const client = await getClient();
+    await client.db().collection<Game>("gamelist").insertOne(req.body);
+    res.status(201).json(newSavedGame);
+  } catch (err) {
+    catchError(err, res);
+  }
+});
 export default appRouter;
